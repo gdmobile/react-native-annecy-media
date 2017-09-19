56e6d8f59054f3e905c342ffc2e89924a1f552a9
schultka <mail@tobias-schultka.com>
2017-09-19 22:13:12 +0800

    added Offer model to index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 //  Copyright © 2017 annecy.media. All rights reserved.
 
 import AnnecyService from './src/AnnecyService.js';
+import Offer from './src/Offer.js';
 import TrackingView from './src/TrackingView.js';
 
 export {
     AnnecyService,
+    Offer,
     TrackingView
 };
